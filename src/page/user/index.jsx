ce5a25021dfa15fff63d23efd4d858e273b369ee
xiongjian <289981872@qq.com>
2020-12-23 10:33:45 +0800

    🐞fix: 111

--- src/page/user/index.jsx
+++ src/page/user/index.jsx
@@ -30,7 +30,7 @@ class UserList extends React.Component{
     }
     loadUserList(){
         _user.getUserList(this.state.pageNum).then(res => {
-            console.log(res)
+            console.log(11111111)
             this.setState(res);
         }, errMsg => {
             this.setState({
